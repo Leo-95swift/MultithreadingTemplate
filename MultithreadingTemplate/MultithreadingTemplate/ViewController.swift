36796d1f80fda90dbf1035e47d7c4459ebe18f0f
Lev-iOS-Dev <159111910+Lev-iOS-Dev@users.noreply.github.com>
2024-03-19 16:05:13 +0400

    initial commit

---
+++ MultithreadingTemplate/MultithreadingTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultithreadingTemplate
+//
+//  Created by Levon Shaxbazyan on 19.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
