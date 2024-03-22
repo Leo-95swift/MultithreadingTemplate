a39fef7cbe48b202a5cb28d2c456165c1730ea34
Lev-iOS-Dev <159111910+Lev-iOS-Dev@users.noreply.github.com>
2024-03-22 09:26:04 +0400

    Завершил задачу 3

---
+++ MultithreadingTemplate/MultithreadingTemplate/Tasks/RecipeViewController.swift
@@ -0,0 +1,55 @@
+//
+//  RecipeViewController.swift
+//  MultithreadingTemplate
+//
+//  Created by Levon Shaxbazyan on 22.03.24.
+//
+
+import UIKit
+
+/// Задача 6 из Task 3.
+final class RecipeViewController: UIViewController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DispatchQueue.global().async {
+            self.thread1()
+        }
+
+        DispatchQueue.global().async {
+            self.thread2()
+        }
+    }
+
+    let resourceASemaphore = DispatchSemaphore(value: 1)
+    let resourceBSemaphore = DispatchSemaphore(value: 1)
+
+    func thread1() {
+        print("Поток 1 пытается захватить Ресурс A")
+        resourceASemaphore.wait() // Захват Ресурса A
+        
+        print("Поток 1 захватил Ресурс A и пытается захватить Ресурс B")
+        Thread.sleep(forTimeInterval: 1) // Имитация работы для демонстрации livelock
+        
+        //resourceBSemaphore.wait() // Попытка захвата Ресурса B, который уже занят Потоком 2
+        print("Поток 1 захватил Ресурс B")
+        
+        //resourceBSemaphore.signal()
+        resourceASemaphore.signal()
+    }
+
+    func thread2() {
+        print("Поток 2 пытается захватить Ресурс B")
+        resourceBSemaphore.wait() // Захват Ресурса B
+        
+        print("Поток 2 захватил Ресурс B и пытается захватить Ресурс A")
+        Thread.sleep(forTimeInterval: 1) // Имитация работы для демонстрации livelock
+        
+       // resourceASemaphore.wait() // Попытка захвата Ресурса A, который уже занят Потоком 1
+        print("Поток 2 захватил Ресурс A")
+        
+        //resourceASemaphore.signal()
+        resourceBSemaphore.signal()
+    }
+}
---
+++ MultithreadingTemplate/MultithreadingTemplate/Tasks/SemaphoreTaskViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SemaphoreTaskViewController.swift
+//  MultithreadingTemplate
+//
+//  Created by Levon Shaxbazyan on 19.03.24.
+//
+
+import UIKit
+
+/*
+ 
+ Наберите весь код руками.
+ Дан сервис, через который записываем фразы в массив используя цикл.
+ 1) Используя DispatchQueue.global() у записывайте фразы через цикл,
+ а не через main thread.
+ 2) Решить проблему потокобезопасности phrases через семафор.
+ 3) Решить проблему, используя actor вместо класса.
+ 
+ */
+
+/// Задача 1 из Task 3.
+final class SemaphoreTaskViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var phrasesService = PhrasesService()
+        
+        DispatchQueue.global().async {
+            for i in 0..<10 {
+                phrasesService.addPhrase("Phrase \(i)")
+            }
+        }
+        
+        // Даем потокам время на завершение работы
+        Thread.sleep(forTimeInterval: 1)
+        
+        // Выводим результат
+        print(phrasesService.phrases)
+    }
+}
+ 
+final class PhrasesService {
+    var phrases: [String] = []
+    let semaphore = DispatchSemaphore(value: 1)
+    
+    func addPhrase(_ phrase: String) {
+        semaphore.wait()
+        phrases.append(phrase)
+        semaphore.signal()
+    }
+}
